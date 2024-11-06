3ddb3afba9a3e9dfe6a8fe200ee0057701b9d1ec
ss3_code <dinhhang440@gmail.com>
2024-11-07 02:22:54 +0700

    change

---
+++ ss1_es6/bai_tap/bai_1.js
@@ -0,0 +1,61 @@
+let courses = [ 
+  { 
+    id: 1, 
+    title: "ReactJS Tutorial", 
+    rating: 4.2, 
+  }, 
+  { 
+    id: 2, 
+    title: "Angular Tutorial", 
+    rating: 2.5, 
+  }, 
+  { 
+    id: 3, 
+    title: "VueJS Tutorial", 
+    rating: 3.8, 
+  }, 
+  { 
+    id: 4, 
+    title: "Java Tutorial", 
+    rating: 4, 
+  }, 
+  { 
+    id: 5, 
+    title: "JavaScript Tutorial", 
+    rating: 3.5, 
+  }, 
+];
+
+// yeu cau 1: Lọc các bài đăng có rating >= 4
+const highRatedCourses = courses.filter(course => course.rating >= 4);
+
+// Xuất ra danh sách các bài đăng thỏa mãn điều kiện
+highRatedCourses.forEach(course => console.log(course.title));
+
+// yeu cau 2: Lọc các bài đăng có rating < 4
+const highRatedCourses2 = courses.filter(course => course.rating < 4);
+highRatedCourses2.forEach(course => `${course.id} - ${course.title} - ${course.rating}`);
+console.log(highRatedCourses2);
+
+// yeu cau 3: Sử dụng cú pháp ES6 đã học viết hàm trả về một mảng mới gộp 2 mảng courses và addedCourses
+let addedCourses = [ 
+    { 
+      id: 6, 
+      title: "PHP Tutorial", 
+      rating: 3, 
+    }, 
+    { 
+      id: 7, 
+      title: "C# Tutorial", 
+      rating: 2, 
+    }, 
+    { 
+      id: 8, 
+      title: "Docker Tutorial", 
+      rating: 3.8, 
+    } 
+  ];
+  const highRatedCourses3 = [...courses,...addedCourses];
+  console.log(highRatedCourses3);
+	
+
